b3cd56852444efedddd8eab5f86881c68c8bc5a5
joneilson pereira <joneilson84@gmail.com>
2024-04-05 16:49:49 -0300

    feat: Criação das Interfaces e Tipagens do Endpoint de Usuário

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,12 +3,13 @@ import { Observable } from 'rxjs';
 import { MaritalStatusEnum } from '../enums/marital-status.enum';
 import { PhoneTypeEnum } from '../enums/phone-type.enum';
 import { AddressTypeEnum } from '../enums/address-type.enum';
+import { UsersListResponse } from '../types/users-list-response';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  private readonly usersList: any[] = [
+  private readonly usersList: UsersListResponse = [
     {
       name: 'Fulano',
       email: 'fulano@hotmail.com',
@@ -140,7 +141,7 @@ export class UsersService {
     },
   ];
 
-  getUsers() {
+  getUsers(): Observable<UsersListResponse> {
     return new Observable((observer) => {
       setTimeout(() => {
         observer.next(this.usersList);
