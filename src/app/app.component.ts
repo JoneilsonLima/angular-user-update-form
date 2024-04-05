a27961777a0f76379a024f96166ac1002a107ebe
joneilson pereira <joneilson84@gmail.com>
2024-04-05 09:33:49 -0300

    service: Criando seriço de usuários

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CountriesService } from './services/countries.service';
 import { StatesService } from './services/states.service';
 import { CitiesService } from './services/cities.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,13 @@ export class AppComponent implements OnInit {
     private readonly _countriesService: CountriesService,
     private readonly _statesService: StatesService,
     private readonly _citiesService: CitiesService,
+    private readonly _usersService: UsersService,
   ) {}
 
   ngOnInit(): void {
     this._countriesService.getCountries().subscribe((value: any) => console.log(value));
     this._statesService.getStates('Brazil').subscribe((value: any) => console.log(value));
     this._citiesService.getCities('Brazil', 'Maranhão').subscribe((value: any) => console.log(value));
+    this._usersService.getUsers().subscribe((value: any) => console.log(value));
   }
 }
