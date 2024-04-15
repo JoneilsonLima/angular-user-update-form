9155530ae61ae9b0b73c55808d82409b761ab189
joneilson pereira <joneilson84@gmail.com>
2024-04-14 23:44:09 -0300

    feat: Finalizando Criação do Componente de Edição de Informações Gerais

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -4,18 +4,24 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from "@angular/material/core";
 
 const allModules = [
   MatCardModule,
   MatTabsModule,
   MatFormFieldModule,
   MatInputModule,
-  MatAutocompleteModule
+  MatAutocompleteModule,
+  MatRadioModule,
+  MatDatepickerModule
 ]
 
 @NgModule({
     imports: allModules,
-    exports: allModules
+    exports: allModules,
+    providers: [provideNativeDateAdapter()],
 })
 export class AngularMaterialModule {
 
