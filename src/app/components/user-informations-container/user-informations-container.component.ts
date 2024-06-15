eae997d7be431806bd53e7e315f53af9cd32385d
joneilson pereira <joneilson84@gmail.com>
2024-06-15 15:16:18 -0300

    Criação do Formulário de Usuário - Criação do Controller

--- src/app/components/user-informations-container/user-informations-container.component.ts
+++ src/app/components/user-informations-container/user-informations-container.component.ts
@@ -1,18 +1,23 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { IUser } from '../../interfaces/user/user.interface';
+import { UserFormController } from './user-form-controller';
 
 @Component({
   selector: 'app-user-informations-container',
   templateUrl: './user-informations-container.component.html',
   styleUrl: './user-informations-container.component.scss'
 })
-export class UserInformationsContainerComponent implements OnChanges {
+export class UserInformationsContainerComponent extends UserFormController implements OnChanges {
   @Input({ required: true }) userSelected: IUser | undefined = {} as IUser;
   @Input({ required: true }) isInEditMode: boolean = false;
 
   public currentTabIndex: number = 0;
 
   ngOnChanges(_: SimpleChanges): void {
-    this.currentTabIndex = 1;
+    this.currentTabIndex = 0;
+
+    if (this.userSelected) {
+      this.fulfillUserForm(this.userSelected);
+    }
   }
 }
