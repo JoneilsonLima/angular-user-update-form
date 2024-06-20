1848a465f51cd3a0e0a723697c536ed2377bab38
joneilson pereira <joneilson84@gmail.com>
2024-06-20 18:33:13 -0300

    feat: Criação dos Validadores e Bindings da Data de Nascimento

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -6,7 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { provideNativeDateAdapter } from "@angular/material/core";
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from "@angular/material/core";
 
 const allModules = [
   MatCardModule,
@@ -21,7 +21,12 @@ const allModules = [
 @NgModule({
     imports: allModules,
     exports: allModules,
-    providers: [provideNativeDateAdapter()],
+    providers: [
+      provideNativeDateAdapter(),
+      {
+        provide: MAT_DATE_LOCALE,
+        useValue: 'pt-BR'
+      }],
 })
 export class AngularMaterialModule {
 
