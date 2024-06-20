3a1da5759d919d1358fd32555009891ce56aec1b
joneilson pereira <joneilson84@gmail.com>
2024-06-20 09:24:44 -0300

    feat: Iniciando a criação da Lógica de Autocomplete da Lista de Países

--- src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CountriesList } from '../../types/countries-list';
 
@@ -7,11 +7,31 @@ import { CountriesList } from '../../types/countries-list';
   templateUrl: './general-informations-edit.component.html',
   styleUrl: './general-informations-edit.component.scss',
 })
-export class GeneralInformationsEditComponent {
+export class GeneralInformationsEditComponent implements OnInit{
   @Input({required: true}) userForm!: FormGroup;
   @Input({required: true}) countriesList: CountriesList = [];
 
+  public countriesListFiltered: CountriesList = [];
+
+  ngOnInit(): void {
+    this.watchCountryFormChangesAndFilter();
+  }
+
   get emailControl(): FormControl {
     return this.userForm.get('generalInformations.email') as FormControl;
+  }
+
+  get countryControl(): FormControl {
+    return this.userForm.get('generalInformations.country') as FormControl;
+  }
+
+  private watchCountryFormChangesAndFilter(): void {
+    this.countryControl?.valueChanges.subscribe(this.filterCountriesList.bind(this));
+  }
+
+  private filterCountriesList(searchTerm: string) {
+    this.countriesListFiltered = this.countriesList.filter((country) => {
+      return country.name.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase().trim());
+    })
   }
 }
