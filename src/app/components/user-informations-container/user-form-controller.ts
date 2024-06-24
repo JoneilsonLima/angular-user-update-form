b407200b51807ce07b117b9e2f7eef7737119ce0
joneilson pereira <joneilson84@gmail.com>
2024-06-23 22:00:55 -0300

    feat: Utilização da Função na Lista de Endereços do Formulário

--- src/app/components/user-informations-container/user-form-controller.ts
+++ src/app/components/user-informations-container/user-form-controller.ts
@@ -7,6 +7,7 @@ import { DependentsList } from "../../types/dependents-list";
 import { convertPtBrDateToDateObj } from "../../utils/convert-pt-br-date-to-date-obj";
 import { preparePhoneList } from "../../utils/prepare-phone-list";
 import { PhoneTypeEnum } from "../../enums/phone-type.enum";
+import { prepareAddressList } from "../../utils/prepare-address-list";
 
 export class UserFormController {
   public userForm!: FormGroup;
@@ -80,17 +81,16 @@ export class UserFormController {
   }
 
   private fulfillAddressList(userAddressList: AddressList) {
-    userAddressList.forEach((address) => {
-      this.addressList.push(
-        this._fb.group({
-          type: [address.type, Validators.required],
-          street: [address.street, Validators.required],
-          complement: [address.complement, Validators.required],
-          country: [address.country, Validators.required],
-          state: [address.state, Validators.required],
-          city: [address.city, Validators.required],
-        })
-      )
+    prepareAddressList(userAddressList, false, (address) => {
+      this.addressList.push(this._fb.group({
+        type: [address.type],
+        typeDescription: [{value: address.typeDescription, disabled: true}],
+        street: [address.street],
+        complement: [address.complement],
+        country: [address.country],
+        state: [address.state],
+        city: [address.city],
+      }));
     })
   }
 
