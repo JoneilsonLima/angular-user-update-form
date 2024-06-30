41bf525fb7e3d757689b77f29f4cb5b15856a3cf
joneilson pereira <joneilson84@gmail.com>
2024-06-30 15:36:38 -0300

    feat: Criação da Interface que Representa o UserForm

--- src/app/services/user-form-raw-value.service.ts
+++ src/app/services/user-form-raw-value.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
+import { IUserForm } from "../interfaces/user-form.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class UserFormRawValueService {
-  userFormRawValue: Object = {};
+  userFormRawValue: IUserForm = {} as IUserForm;
 
 }
